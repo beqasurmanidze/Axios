eb1c654082c81fddd99038261a2ff353e68d6544
Your Name <beqasurmana2900@gmail.com>
2024-09-23 20:21:42 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setUsers(data);
+        if (!response.ok) throw new Error("networking response was NOT okay");
+      } catch (error) {
+        console.error("this is my error:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
 
   const fetchUserById = (id) => {
     console.log(id);
